7ac297e235374d921cd46b200b3d44e1a1285271
Thuy Tien <lethuytien2468@gmail.com>
2026-04-08 21:48:32 +0700

    update transform script

---
+++ elt-pipeline/sql/dim_customers.sql
@@ -0,0 +1,55 @@
+CREATE TABLE IF NOT EXISTS analytics.dim_customers (
+    customer_sk STRING NOT NULL,
+    customer_id STRING,
+    customer_name STRING,
+    city STRING,
+    run_id STRING,
+    load_time TIMESTAMP,
+    is_current BOOL,
+    PRIMARY KEY (customer_sk) NOT ENFORCED
+);
+
+MERGE analytics.dim_customers d
+USING (
+    SELECT DISTINCT c.customer_id
+    FROM raw1.customers c
+    JOIN analytics.dim_customers d2
+      ON c.customer_id = d2.customer_id
+     AND d2.is_current = TRUE
+    WHERE c.run_id = '{{ run_id }}'
+      AND (
+            d2.customer_name != TRIM(c.customer_name)
+         OR d2.city != c.city
+      )
+) AS changed
+ON d.customer_id = changed.customer_id
+AND d.is_current = TRUE
+WHEN MATCHED THEN
+    UPDATE SET is_current = FALSE;
+
+-- only insert new records or changed records
+
+INSERT INTO analytics.dim_customers (
+    customer_sk,
+    customer_id,
+    customer_name,
+    city,
+    run_id,
+    load_time,
+    is_current
+)
+SELECT
+    GENERATE_UUID() AS customer_sk,
+    c.customer_id,
+    TRIM(c.customer_name),
+    c.city,
+    c.run_id,
+    CURRENT_TIMESTAMP(),
+    TRUE
+FROM raw1.customers c
+WHERE c.run_id = '{{ run_id }}'
+  AND NOT EXISTS (
+    SELECT 1 FROM analytics.dim_customers d
+    WHERE d.customer_id = c.customer_id
+      AND d.is_current = TRUE
+  );
---
+++ elt-pipeline/sql/dim_targets_orders.sql
@@ -0,0 +1,55 @@
+CREATE TABLE IF NOT EXISTS analytics.dim_targets_orders (
+    order_sk STRING NOT NULL,
+    customer_sk STRING,
+    customer_id STRING,
+    ontime_target FLOAT64,
+    infull_target FLOAT64,
+    otif_target FLOAT64,
+    run_id STRING,
+    load_time TIMESTAMP,
+    is_current BOOL,
+    PRIMARY KEY (order_sk) NOT ENFORCED,
+    FOREIGN KEY (customer_sk) REFERENCES analytics.dim_customers(customer_sk) NOT ENFORCED
+);
+
+MERGE analytics.dim_targets_orders d
+USING (
+    SELECT DISTINCT t.customer_id
+    FROM raw1.targets_orders t
+    JOIN analytics.dim_targets_orders d2
+      ON t.customer_id = d2.customer_id
+     AND d2.is_current = TRUE
+    WHERE t.run_id = '{{ run_id }}'
+      AND (
+            d2.ontime_target != t.ontime_target
+         OR d2.infull_target != t.infull_target
+         OR d2.otif_target   != t.otif_target
+      )
+) AS changed
+ON d.customer_id = changed.customer_id
+AND d.is_current = TRUE
+WHEN MATCHED THEN
+    UPDATE SET is_current = FALSE;
+
+INSERT INTO analytics.dim_targets_orders (order_sk, customer_sk, customer_id, ontime_target, infull_target, otif_target, run_id, load_time, is_current)
+SELECT
+    GENERATE_UUID() AS order_sk,
+    d.customer_sk,
+    o.customer_id,
+    o.ontime_target,
+    o.infull_target,
+    o.otif_target,
+    o.run_id,
+    CURRENT_TIMESTAMP(),
+    TRUE AS is_current
+FROM raw1.targets_orders o
+LEFT JOIN analytics.dim_targets_orders d
+  ON o.customer_id = d.customer_id
+ AND d.is_current = TRUE
+WHERE o.run_id = '{{ run_id }}'
+  AND NOT EXISTS (                     
+    SELECT 1 FROM analytics.dim_targets_orders d
+    WHERE d.customer_id = o.customer_id
+      AND d.is_current = TRUE
+  );
+
